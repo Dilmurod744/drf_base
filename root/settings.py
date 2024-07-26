4838a99279627b29a130c65aa2b7709780829418
Dilmurod744 <dilmurod.xolmatovrg@gmail.com>
2024-07-26 17:04:09 +0500

    soccial auth

--- root/settings.py
+++ root/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.linkedin_oauth2',
+    'allauth.socialaccount.providers.instagram',
 
     # my-apps
     "apps",
